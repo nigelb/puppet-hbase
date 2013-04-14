30719124e6ee71cedd41475f6ff438856a95e7c7
nigelb <nigel.blair+github@gmail.com>
2013-04-15 09:50:22 +1000

    Parameterized param class

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,63 +1,20 @@
 # /etc/puppet/modules/hbase/manafests/params.pp
 
-class hbase::params {
-
+class hbase::params ( 
+	$version =  "0.95-SNAPSHOT",
+ 	$hadoop_user =  "hduser",
+ 	$hadoop_group =  "hadoop",
+	$master = "master.hadoop",
+	$slaves = ["slave01.hadoop", "slave02.hadoop"] 
+    	$namenode =  "${master}",
+	$hdfsport = "8020",
+	$rootdir = "hbase",
+	$java_home = "${java::params::java_base}/jdk${java::params::java_version}",
+	$hadoop_base = "/opt/hadoop",
+	$hadoop_conf = "${hadoop_base}/hadoop/conf",
+	$hbase_base = "/opt/hbase",
+	$hbase_conf = "${hbase_base}/hbase/conf",
+    	$hadoop_user_path = "/home/${hadoop_user}",
+{
 	include java::params
-
-	$version = $::hostname ? {
-		default			=> "0.95-SNAPSHOT",
-	}
-
- 	$hadoop_user = $::hostname ? {
-		default			=> "hduser",
-	}
- 
- 	$hadoop_group = $::hostname ? {
-		default			=> "hadoop",
-	}
-        
-	$master = $::hostname ? {
-		default			=> "master.hadoop",
-	}
- 
-	$slaves = $::hostname ? {
-		default			=> ["slave01.hadoop", "slave02.hadoop"] 
-	}
- 
-    $namenode =  $::hostname ? {
-		default			=> "${master}",
-	}            
- 
-	$hdfsport = $::hostname ? {
-		default			=> "8020",
-	}
- 
-	$rootdir = $::hostname ? {
-		default			=> "hbase",
-	}
- 
-	$java_home = $::hostname ? {
-		default			=> "${java::params::java_base}/jdk${java::params::java_version}",
-	}
-
-	$hadoop_base = $::hostname ? {
-		default			=> "/opt/hadoop",
-	}
- 
-	$hadoop_conf = $::hostname ? {
-		default			=> "${hadoop_base}/hadoop/conf",
-	}
- 
-	$hbase_base = $::hostname ? {
-		default			=> "/opt/hbase",
-	}
- 
-	$hbase_conf = $::hostname ? {
-		default			=> "${hbase_base}/hbase/conf",
-	}
- 
-    $hadoop_user_path = $::hostname ? {
-		default			=> "/home/${hadoop_user}",
-	}             
-
 }
